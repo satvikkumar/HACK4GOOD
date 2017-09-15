0ba41db446f775d6973918d6c714391565b4e08c
Satvik Kumar <satvik.kumar97@gmail.com>
2017-09-15 12:20:02 +0000

    Update ser.sh

--- ser.sh
+++ ser.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect
-for {set i 1} {$i < 3} {incr i 1} {
+for {set i 1} {$i < 2} {incr i 1} {
 
 set prompt {\$ $};
 
@@ -7,39 +7,40 @@ spawn bash
 expect "satvik@pes:~$ "
 send "cd /home/satvik/darknet\r"
 
-expect "satvik@pes:~$ "
-send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image1.jpg"
-expect "satvik@pes:~$ "
+expect "satvik@pes:~/darknet$ "
+send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image1.jpg > data1.txt\r"
+expect "satvik@pes:~/darknet$ "
 send "cp /home/satvik/darknet/predictions.png /home/satvik/predictions1.png\r"
 
-expect "satvik@pes:~$ "
-send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image2.jpg"
-expect "satvik@pes:~$ "
+expect "satvik@pes:~/darknet$ "
+send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image2.jpg > data2.txt\r"
+expect "satvik@pes:~/darknet$ "
 send "cp /home/satvik/darknet/predictions.png /home/satvik/predictions2.png\r"
 
-expect "satvik@pes:~$ "
-send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image3.jpg"
-expect "satvik@pes:~$ "
+expect "satvik@pes:~/darknet$ "
+send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image3.jpg > data3.txt\r"
+expect "satvik@pes:~/darknet$ "
 send "cp /home/satvik/darknet/predictions.png /home/satvik/predictions3.png\r"
 
-expect "satvik@pes:~$ "
-send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image4.jpg"
-expect "satvik@pes:~$ "
+expect "satvik@pes:~/darknet$ "
+send "./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights data/image4.jpg > data4.txt\r"
+expect "satvik@pes:~/darknet$ "
 send "cp /home/satvik/darknet/predictions.png /home/satvik/predictions4.png\r"
 
-expect "satvik@pes:~$ "
-send "grep -c '\<\(bus\|car\)\>' /home/satvik/data1.txt > /home/satvik/num.txt\r"
+expect "satvik@pes:~/darknet$ "
+send "grep -c '\\<\\(bus\\|car\\)\\>' data1.txt > num.txt\r"
 
-expect "satvik@pes:~$ "
-send "grep -c '\<\(bus\|car\)\>' /home/satvik/data2.txt >> /home/satvik/num.txt\r"
+expect "satvik@pes:~/darknet$ "
+send "grep -c '\\<\\(bus\\|car\\)\\>' data2.txt >> num.txt\r"
 
-expect "satvik@pes:~$ "
-send "grep -c '\<\(bus\|car\)\>' /home/satvik/data3.txt >> /home/satvik/num.txt\r"
+expect "satvik@pes:~/darknet$ "
+send "grep -c '\\<\\(bus\\|car\\)\\>' data3.txt >> num.txt\r"
 
-expect "satvik@pes:~$ "
-send "grep -c '\<\(bus\|car\)\>' /home/satvik/data4.txt >> /home/satvik/num.txt\r"
- 
-spawn lftp
+expect "satvik@pes:~/darknet$ "
+send "grep -c '\\<\\(bus\\|car\\)\\>' data4.txt >> num.txt\r"
+
+expect "satvik@pes:~/darknet$ " 
+send "lftp\r"
 
 expect "lftp :~> "
 send "set ssl:verify-certificate no\r"
@@ -63,9 +64,14 @@ expect "lftp epiz_20655524@ftp.epizy.com:/htdocs/images> "
 send "put /home/satvik/predictions4.png\r"
 
 expect "lftp epiz_20655524@ftp.epizy.com:/htdocs/images> "
-send "cd htdocs/data\r"
+send "cd ..\r"
+
+expect "lftp epiz_20655524@ftp.epizy.com:/htdocs> "
+send "cd data\r"
+
 expect "lftp epiz_20655524@ftp.epizy.com:/htdocs/data> "
 send "put /home/satvik/num.txt\r"
+
 expect "lftp epiz_20655524@ftp.epizy.com:/htdocs/data> "
 send "exit"
 }
